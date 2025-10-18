30b309d9971ea16c8b5584ffa61c821679c1aedb
Ahmed Osama <168051526+AhmedOsama143@users.noreply.github.com>
2025-10-18 03:51:01 +0300

    Add files via upload

---
+++ Next-last-project/app/(main)/about/page.js
@@ -0,0 +1,16 @@
+export default function AboutPage() {
+  return (
+    <section className="text-center mt-16 max-w-3xl mx-auto">
+      <h1 className="text-4xl font-bold text-blue-700 mb-6">About Us</h1>
+      <p className="text-gray-700 text-lg mb-4">
+        At <strong>MY Next Store</strong>, we believe in providing the best
+        online shopping experience.
+      </p>
+      <p className="text-gray-700">
+        Our goal is to provide top-notch products with quick delivery and a
+        seamless shopping experience. We’re dedicated to making online shopping
+        easy, smart, and satisfying.
+      </p>
+    </section>
+  );
+}
