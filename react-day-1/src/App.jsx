938f64490170e56184bca96e5cc2197b6922e796
Ahmed Osama <168051526+AhmedOsama143@users.noreply.github.com>
2025-10-19 20:13:19 +0300

    Add files via upload

---
+++ react-day-1/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Card from "./components/Card/Card";
+import Footer from "./Components/Footer/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Card />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
